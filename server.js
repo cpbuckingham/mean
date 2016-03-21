53779445ab4445cf67df1cb72109bac02fa6cafe
Cam <cameron.p.buckingham@gmail.com>
2016-03-21 17:05:07 -0500

    populated HTML with header and title

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require ('express');
 var app = express();
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000)
 console.log("Running ... then you better go catch it!");
