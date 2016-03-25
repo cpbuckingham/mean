3c155b74f94e2e6df06e0dd85b5d46c58f183b5f
Cam <cameron.p.buckingham@gmail.com>
2016-03-25 09:32:51 -0500

    dumbieData

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,23 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
+
+    person1={
+      name: "Tim",
+      email: "tim@gmail.com",
+      number: "111-111-1111"
+    }
+    person2={
+      name: "John",
+      email: "john@gmail.com",
+      number: "222-111-1111"
+    }
+    person3={
+      name: "Ally",
+      email: "ally@gmail.com",
+      number: "333-111-1111"
+    }
+
+    var contactlist= [person1, person2, person3];
+
 }]);
