88d3cb107636fae2db7010f05c82f2e55402ec93
Cam <cameron.p.buckingham@gmail.com>
2016-03-25 09:44:32 -0500

    json data passed from server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,26 +2,9 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-    $http.get('/contactlist');
-
-    person1={
-      name: "Tim",
-      email: "tim@gmail.com",
-      number: "111-111-1111"
-    }
-    person2={
-      name: "John",
-      email: "john@gmail.com",
-      number: "222-111-1111"
-    }
-    person3={
-      name: "Ally",
-      email: "ally@gmail.com",
-      number: "333-111-1111"
-    }
-
-    var contactlist= [person1, person2, person3];
-    $scope.contactlist = contactlist;
-    //$scope allows use to ues contactlist in HTML
+    $http.get('/contactlist').success(function (response) {
+        console.log("I got the data");
+        $scope.contactlist = response;
+    });
 
 }]);
--- server.js
+++ server.js
@@ -5,6 +5,25 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist', function(req, res){
   console.log("Recieved the GET request")
+  person1={
+    name: "Tim",
+    email: "tim@gmail.com",
+    number: "111-111-1111"
+  }
+  person2={
+    name: "John",
+    email: "john@gmail.com",
+    number: "222-111-1111"
+  }
+  person3={
+    name: "Ally",
+    email: "ally@gmail.com",
+    number: "333-111-1111"
+  }
+
+  var contactlist= [person1, person2, person3];
+  res.json(contactlist)
+  //respond to the get request by sending back the data
 });
 
 app.listen(3000)
