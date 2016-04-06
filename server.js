123af8d24e9fe7a98303ccd45cde04da7db9fc05
Cam <cameron.p.buckingham@gmail.com>
2016-04-06 09:17:32 -0500

    refactored and completed

--- server.js
+++ server.js
@@ -2,51 +2,45 @@ var express = require ('express');
 var app = express();
 var mongojs = require ('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
-var austinSoBE = require('body-parser');
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
-app.use(austinSoBE.json());
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res){
-  console.log("Recieved the GET request")
     db.contactlist.find(function(err, docs){
-      console.log(docs);
       res.json(docs);
+      });
     });
-  //respond to the get request by sending back the data
-});
 
 app.post('/contactlist', function(req, res){
-  console.log(req.body);
   db.contactlist.insert(req.body, function(err, doc){
     res.json(doc);
-  })
-});
+      })
+    });
 
 app.delete('/contactlist/:id', function(req, res){
   var id=req.params.id;
-  console.log(id);
   db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
     res.json(doc);
+    });
   });
-});
 
 app.get('/contactlist/:id', function(req, res){
   var id = req.params.id;
   db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
     res.json(doc);
+    });
   });
-});
 
 app.put('/contactlist/:id', function(req, res){
   var id = req.params.id;
-  console.log(req.body.name);
   db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
   update: {$set: {name: req.body.name, email: req.body.email, numbers: req.body.numbers}},
   new: true}, function(err, doc){
     res.json(doc);
-});
-});
+    });
+  });
 
 app.listen(3000)
 console.log("Running ... then you better go catch it!");
